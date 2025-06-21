b492b8cca25811d1e4a7d7c1f9f7930fbd8ad0ad
萌新杰少 <1250422131@qq.com>
2025-06-21 21:08:23 +0800

    移除未使用变量

--- src/renderer/src/utils/router.tsx
+++ src/renderer/src/utils/router.tsx
@@ -1,27 +1,13 @@
 import {
   createHashRouter,
-  redirect,
 } from "react-router";
 
 import HomePage from "@renderer/components/home/HomePage";
 import Settings from "@renderer/Setting";
 import Devices from "@renderer/tools/Devices";
-import { useAdbStore } from "@renderer/store/adbStore";
 import AllConnect from "@renderer/tools/AllConnect";
 
 
-const checkAdbLoader = async () => {
-  // try {
-  //   const result = await useAdbStore.getState().updateCheckAdbState();
-  //   if (result.success && result.count > 0) {
-  //     return null;
-  //   } else {
-  //     throw redirect('/');
-  //   }
-  // } catch (error) {
-  //   throw redirect('/');
-  // }
-}
 
 const router = createHashRouter([
   {
